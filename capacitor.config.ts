a54ce15add870d16bb3888d2f061d21ca45952aa
adeelahmed <adeelahmedhashmi.aah@gmail.com>
2025-11-22 16:40:07 +0000

    add capacitor in project to build apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spaceshooter.game',
+  appName: 'SpaceShooter',
+  webDir: 'dist'
+};
+
+export default config;
