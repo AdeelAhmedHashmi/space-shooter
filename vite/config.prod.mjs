9a31c1ce642accd947c257d1c148d209c9a2ad7b
adeelahmedhashmi <adeelahmedhashmi.aah@gmail.com>
2025-11-23 11:00:53 +0500

    feat: add PWA support with service worker registration and update build configuration

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 const phasermsg = () => {
     return {
@@ -15,7 +16,35 @@ const phasermsg = () => {
 
 export default defineConfig({
     base: "./",
-    plugins: [react(), phasermsg()],
+    plugins: [
+        react(),
+        phasermsg(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["favicon.ico"],
+            manifest: {
+                name: "Space Shooter Game",
+                short_name: "SpaceShooter",
+                theme_color: "#000000",
+                background_color: "#1f1f1fff",
+                display: "standalone",
+                orientation: "portrait",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "icons/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
     logLevel: "warning",
     build: {
         rollupOptions: {
